50325f4736bee631f6e6da6416c734ea62947a3a
livmeierx <liv.meier13@gmail.com>
2022-06-15 17:43:55 -0400

    updated header and container items

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,21 @@ var APIKey = "bf35e76068825d20a4cff09151512725";
 var currentCity = "";
 var searchCity = "";
 
-var currentWeather = (event) => {
+var noErrors = (response) => {
     
+}
+
+//function to retrieve and display current conditions 
+var currentWeather = (event) => {
+    // let city = $("#city-search").val();
+    // currentCity = $("#search-city").val();
+    // //fetch from API
+    // let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + APIKey;
+    // fetch(queryURL)
+    // .then(response) => {
+    //     return response.json();
+    // }
+    // .then(response) => {
+
+    // }
 }
