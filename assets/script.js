f27694f5ae650876e24dc935c0874496a3be8bda
livmeierx <liv.meier13@gmail.com>
2022-06-15 17:09:12 -0400

    added containers to hmtl and set up header styling

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 // assign a unique API to variable and set other variable
 var APIKey = "bf35e76068825d20a4cff09151512725";
 var currentCity = "";
+var searchCity = "";
 
 var currentWeather = (event) => {
     
