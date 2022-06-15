f2b4fdccb62a2c333bf76cf02282d2de3b687a13
livmeierx <liv.meier13@gmail.com>
2022-06-15 18:04:50 -0400

    started first function

--- assets/script.js
+++ assets/script.js
@@ -4,20 +4,34 @@ var currentCity = "";
 var searchCity = "";
 
 var noErrors = (response) => {
-    
+    if (response.ok) {
+        throw Error(response.statusText);
+    }
+    return response;
 }
 
 //function to retrieve and display current conditions 
 var currentWeather = (event) => {
-    // let city = $("#city-search").val();
-    // currentCity = $("#search-city").val();
-    // //fetch from API
-    // let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + APIKey;
-    // fetch(queryURL)
-    // .then(response) => {
-    //     return response.json();
-    // }
-    // .then(response) => {
+    let city = $("#city-search").val();
+    currentCity = $("#search-city").val();
 
-    // }
+    //fetch from API
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + APIKey;
+    fetch(queryURL)
+    .then(noErrors)
+    .then((response) => {
+        return response.json();
+    })
+    //save to storage 
+    .then((response) => {
+        saveCity(city);
+        $("#search-error").text("");
+        //create icon using api
+        let currentWeatherData="https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+        //use moment.js
+        let timeUTC = response.dt;
+        let timeZoneUpdate = response.timezone;
+        let timeZoneHours = timeZoneUpdate / 60 / 60;
+        
+    })
 }
