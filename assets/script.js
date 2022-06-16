8f24e5dae2c4ebdfee09e3368fd825c786c3a1f0
livmeierx <liv.meier13@gmail.com>
2022-06-15 20:48:37 -0400

    completed currentWeather function

--- assets/script.js
+++ assets/script.js
@@ -16,7 +16,7 @@ var currentWeather = (event) => {
     currentCity = $("#search-city").val();
 
     //fetch from API
-    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + APIKey;
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=" + APIKey;
     fetch(queryURL)
     .then(noErrors)
     .then((response) => {
@@ -27,11 +27,61 @@ var currentWeather = (event) => {
         saveCity(city);
         $("#search-error").text("");
         //create icon using api
-        let currentWeatherData="https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+        let currentWeatherData = "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
         //use moment.js
         let timeUTC = response.dt;
         let timeZoneUpdate = response.timezone;
         let timeZoneHours = timeZoneUpdate / 60 / 60;
-        
+        let currentTime = moment.unix(timeUTC).utc().utcOffset(timeZoneHours);
+
+        getCities();
+
+        getForecast();
+        $("#header-text").text(response.name);
+
+        //get results with HTML
+        let currentWeatherHTML = '
+            <h2>${response.name} ${currentMoment.format("(MM/DD/YY)")}<img src="${currentWeatherIcon}"></img></h2>
+            <ul>
+                <li>Temperature: ${response.main.temp} &8457;</li>
+                <li>Humidty: ${response.main.humdity} %</li>
+                <li>Wind Speed: ${response.wind.speed} mph</li>
+                <li id="uvIndex">UV Index:</li>
+            </ul>';
+        //append results
+        $("#current-weather").html(currentWeatherHTML);
+
+        let latitude = response.coord.lat;
+        let longitude = response.coord.lon;
+        let uvQuery = "api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIKey;
+
+        fetch(uvQuery)
+        .then(noErrors)
+        .then ((response) => {
+            return response.json();
+        })
+        .then((response) => {
+            let uvIndex = response.value;
+            $("#uvIndex").html('UV Index: <span id="uvVal"> ${uvIndex}</span>');
+            if (uvIndex>=0 && uvIndex<3){
+                $("#uvVal").attr("class", "uv-favorable");
+            } else if (uvIndex>=3 && uvIndex<8){
+                $("#uvVal").attr("class", "uv-moderate");
+            } else if (uvIndex>=8){
+                $("#uvVal").attr("class", "uv-severe");
+            }
+        });
     })
+}
+
+var getForecast = (event) => {
+
+}
+
+var getCities = () => {
+
+}
+
+var saveCity = (newCity) => {
+
 }
