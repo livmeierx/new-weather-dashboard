7302d18ff45aee5d5a8e75bf7fbd0f12b19d07b3
livmeierx <liv.meier13@gmail.com>
2022-06-16 14:47:09 -0400

    fixed error with strings

--- assets/script.js
+++ assets/script.js
@@ -37,19 +37,19 @@ var currentWeather = (event) => {
         getCities();
 
         getForecast();
-        $("#header-text").text(response.name);
+        $('#header-text').text(response.name);
 
         //get results with HTML
-        let currentWeatherHTML = "
-            <h2>${response.name} ${currentMoment.format("(MM/DD/YY)")}<img src="${currentWeatherIcon}"></img></h2>
-            <ul>
-                <li>Temperature: ${response.main.temp} &8457;</li>
-                <li>Humidty: ${response.main.humdity} %</li>
+        let currentWeatherHTML = `
+            <h3>${response.name} ${currentMoment.format("(MM/DD/YY)")}<img src="${currentWeatherIcon}"></h3>
+            <ul class="list-unstyled">
+                <li>Temperature: ${response.main.temp}&#8457;</li>
+                <li>Humidity: ${response.main.humidity}%</li>
                 <li>Wind Speed: ${response.wind.speed} mph</li>
                 <li id="uvIndex">UV Index:</li>
-            </ul>;
+            </ul>`;
         //append results
-        $("#current-weather").html(currentWeatherHTML);
+        $('#current-weather').html(currentWeatherHTML);
 
         let latitude = response.coord.lat;
         let longitude = response.coord.lon;
@@ -86,9 +86,9 @@ var getFiveDay = (event) => {
         return response.json();
     })
     .then((response) => {
-        let fiveDayHMTL = "
-        <h2>Five Day Forecast:</h2>
-        <div id="fiveDayUl" class="d-inline-flex flex-wrap"></div>";
+        let getFiveDay = `
+        <h2>5-Day Forecast:</h2>
+        <div id="fiveDayForecastUl" class="d-inline-flex flex-wrap ">`;
 
         for (let i = 0; i < response.list.length; i++) {
             let dayInfo = response.list[i];
@@ -96,25 +96,25 @@ var getFiveDay = (event) => {
             let dayTimeZone = response.city.timezone;
             let dayZoneHours = dayTimeZone / 60 / 60;
             let thisMoment = moment.unix(dayUTC).utc().utcOffset(dayZoneHours);
-            let iconURL = "https://openweathermap.org/img/w/" + dayData.weather[0].icon + ".png";
+            let iconURL = "https://openweathermap.org/img/w/" + dayInfo.weather[0].icon + ".png";
 
             if (thisMoment.format("HH:mm:ss") === "11:00:00" || thisMoment.format("HH:mm:ss") === "12:00:00" || thisMoment.format("HH:mm:ss") === "13:00:00") {
-                fiveDayHMTL += "
+                getFiveDay += `
                 <div class="weather-card card m-2 p0">
                     <ul class="list-unstyled p-3">
                         <li>${thisMoment.format("MM/DD/YY")}</li>
-                        <li class="weather-icon"><img src="$(iconURL}"></li>
+                        <li class="weather-icon"><img src="${iconURL}"></li>
                         <li>Temp: ${dayInfo.main.temp}&#8457;</li>
                         <br>
-                        <li>Humidty: ${dayInfo.main.humidty} %</li>
+                        <li>Humidity: ${dayInfo.main.humidity}%</li>
                     </ul>
-                </div>";
-            }    
+                </div>`;
+            }  
         }
         fiveDayHMTL += '</div>';
-        $('#five-day-forecast').html(fiveDayHMTL);
-    })
-};
+        $('#five-day').html(fiveDayHMTL)
+    });
+}
 
 //function to get list of cities
 var getCities = () => {
